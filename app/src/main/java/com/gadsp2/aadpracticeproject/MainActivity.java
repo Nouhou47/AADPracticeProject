b92de02f9449754237fa60289903908008651a69
JellyBean <nouhoundach@gmail.com>
2020-09-11 12:59:18 +0100

    "(LeaderBoard & SubmitProject) activities added but implementation not finished"

--- app/src/main/java/com/gadsp2/aadpracticeproject/MainActivity.java
+++ app/src/main/java/com/gadsp2/aadpracticeproject/MainActivity.java
@@ -2,29 +2,24 @@ package com.gadsp2.aadpracticeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    int secondToLaunchNextActivity = 3;
-    CountDownTimer countDownTimer;
+
+    // Helper method to launch leaderBoard activity
+    public void openLeaderBordActivity(View view) {
+        System.out.println("Starting method that opens leader board activity");
+        Intent intent = new Intent(this, LeaderBoard.class);
+        startActivity(intent);
+        System.out.println("Method that opens leader board activity finished!!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        countDownTimer = new CountDownTimer(10,300) {
-            @Override
-            public void onTick(long l) {
-
-            }
-
-            @Override
-            public void onFinish() {
-
-            }
-        };
     }
 }
